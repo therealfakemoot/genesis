603cfe6fd8c34920f5a69e08cddd7564c6c24140
Nick Dumas <nicholas.dumas@homes.com>
2019-02-28 16:05:42 +0000

    Fixed some really dumb problems with the d3 code.
    I was using Max twice, resulting in 0 thresholds, which means nothing
    got rendered.

--- render.go
+++ render.go
@@ -7,6 +7,12 @@ import (
 
 var rawTemplate = `
 <!DOCTYPE html>
+<style>
+body {
+	background: black;
+}
+</style>
+<body>
 <svg width="{{ $.Width }}" height="{{ $.Height }}" stroke="#fff" stroke-width="0.5"></svg>
 <script src="https://d3js.org/d3.v4.min.js"></script>
 <script src="https://d3js.org/d3-hsv.v0.1.min.js"></script>
@@ -27,17 +33,21 @@ d3.json("/map?width={{ $.Width }}&height={{ $.Height }}&seed={{ $.Seed }}&min={{
 		throw error;
 	}
 
-	svg.selectAll("path")
-	.data(d3.contours()
+	let thresholds = d3.range({{ $.Domain.Min }}, {{ $.Domain.Max }} + 1, 50)
+
+	let contours = d3.contours()
 	.size([terrain.width, terrain.height])
-	.thresholds(d3.range({{ $.Domain.Max }}, {{ $.Domain.Max }}, 5))
-	(terrain.values))
+	.thresholds(thresholds)(terrain.values)
+
+	svg.selectAll("path")
+	.data(contours)
 	.enter().append("path")
 	.attr("d", d3.geoPath(d3.geoIdentity().scale(width / terrain.width)))
 	.attr("fill", function(d) { return color(d.value); });
 });
 
 </script>
+</body>
 `
 
 var TopoMap = template.Must(template.New("terrain").Parse(rawTemplate))
