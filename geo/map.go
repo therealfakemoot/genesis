94fe08c2632538bc13f3bf5d886db83d988bf14f
Nick Dumas <nicholas.dumas@homes.com>
2019-03-04 22:40:20 +0000

    Tentative work towards adding gonum/plot support.

--- geo/map.go
+++ geo/map.go
@@ -15,6 +15,36 @@ type Map struct {
 	Points [][]float64
 }
 
+func (m Map) Dims() (int, int) {
+	return m.Width, m.Height
+}
+
+func (m Map) Z(c, r int) float64 {
+	return m.Points[c][r]
+}
+
+func (m Map) X(c int) float64 {
+	if c > m.Width {
+		panic("out of bounds")
+	}
+	return float64(c)
+}
+
+func (m Map) Y(r int) float64 {
+	if r > m.Height {
+		panic("out of bounds")
+	}
+	return float64(r)
+}
+
+func (m Map) Min() float64 {
+	return m.Domain.Min
+}
+
+func (m Map) Max() float64 {
+	return m.Domain.Max
+}
+
 func New(x, y, seed int, d Q.Domain) (m Map) {
 	m.Seed = seed
 	m.Points = make([][]float64, y)
