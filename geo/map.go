34706812e697c02d15ddf3e4c8e1c9c116ca9f44
Nick Dumas <nicholas.dumas@homes.com>
2019-03-05 14:31:20 +0000

    Separating noise generation into its own file.
    Gonna start adding types and variants of these functions.

--- geo/map.go
+++ geo/map.go
@@ -1,7 +1,6 @@
 package geo
 
 import (
-	noise "github.com/ojrac/opensimplex-go"
 	// log "github.com/sirupsen/logrus"
 
 	Q "github.com/therealfakemoot/go-quantize"
@@ -55,27 +54,4 @@ func New(x, y, seed int, d Q.Domain) (m Map) {
 	m.Points = Noise(m)
 
 	return m
-}
-
-func Noise(m Map) (points [][]float64) {
-	// This is important. Adding the noise values together means the input domain grows.
-	input := Q.Domain{Min: -3, Max: 3}
-	n := noise.New(int64(m.Seed))
-
-	x, y := m.Width, m.Height
-
-	for i := 0; i < y; i++ {
-		row := make([]float64, x)
-		for j := 0; j < x; j++ {
-			v := n.Eval2(float64(j)*0.001, float64(i)*0.001) +
-				n.Eval2(float64(j)*0.05, float64(i)*0.05) +
-				(0.25 * n.Eval2(float64(j)*0.1, float64(i)*0.1))
-
-			row[j] = v
-		}
-		quantized := Q.QuantizeAll(row, input, m.Domain)
-
-		points = append(points, quantized)
-	}
-	return points
 }
