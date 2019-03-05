c57a48c2a0423592b2090fc4d6677f16681a78d3
Nick Dumas <nicholas.dumas@homes.com>
2019-03-05 14:58:38 +0000

    NoiseOpts will hold all the parameters for fine tuning the noise.
    This will get serialized alongside every map it generates. In theory,
    this should allow for deterministic and reliable reproduction of maps.

--- geo/noise.go
+++ geo/noise.go
@@ -7,25 +7,33 @@ import (
 )
 
 type NoiseOpts struct {
+	Alpha []float64
 }
+
+var (
+	NoiseDefaults = NoiseOpts{
+		Alpha: []float64{0.001, 0.05, 0.1},
+	}
+)
 
 func Noise(m Map) (points [][]float64) {
-	return NoiseComplex(m)
+	return NoiseComplex(m, NoiseDefaults)
 }
 
-func NoiseComplex(m Map) (points [][]float64) {
+func NoiseComplex(m Map, no NoiseOpts) (points [][]float64) {
 	// This is important. Adding the noise values together means the input domain grows.
 	input := Q.Domain{Min: -3, Max: 3}
 	n := noise.New(int64(m.Seed))
 
 	x, y := m.Width, m.Height
+	alphaFine, alphaMid, alphaCoarse := no.Alpha[0], no.Alpha[1], no.Alpha[2]
 
 	for i := 0; i < y; i++ {
 		row := make([]float64, x)
 		for j := 0; j < x; j++ {
-			v := n.Eval2(float64(j)*0.001, float64(i)*0.001) +
-				n.Eval2(float64(j)*0.05, float64(i)*0.05) +
-				(0.25 * n.Eval2(float64(j)*0.1, float64(i)*0.1))
+			v := n.Eval2(float64(j)*alphaFine, float64(i)*alphaFine) +
+				n.Eval2(float64(j)*alphaMid, float64(i)*alphaMid) +
+				(0.25 * n.Eval2(float64(j)*alphaCoarse, float64(i)*alphaCoarse))
 
 			row[j] = v
 		}
