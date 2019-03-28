e943440dffc91ecdd9ef16fd82c3da8ad40f6cbe
Nick Dumas <nicholas.dumas@homes.com>
2019-03-28 13:46:06 +0000

    Go app now serves react code.
    React acts as the UI and will let the user play with variables. The real
    hard part will be building all the different UI variants.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 	// "flag"
 
 	"github.com/go-chi/chi"
@@ -18,14 +20,18 @@ func main() {
 	r.Use(chi_mid.Logger)
 	r.Use(chi_mid.Recoverer)
 
-	r.Get("/map/html", render.ServeHTML)
+	r.Get("/", ServeMap)
+
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "public/static")
+	FileServer(r, "/static", http.Dir(filesDir))
 
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.MapCtx)
 		// r.Use(chi_mid.Throttle(5))
 
-		r.Post("/map/png", render.ServePNG)
-		r.Post("/map/json", render.ServeJSON)
+		r.Post("/map/png/", render.ServePNG)
+		r.Post("/map/json/", render.ServeJSON)
 
 	})
 	log.Fatal(http.ListenAndServe(":8080", r))
--- cmd/web/serve.go
+++ cmd/web/serve.go
@@ -1,34 +1,42 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
-
-	geo "github.com/therealfakemoot/genesis/geo"
-	middleware "github.com/therealfakemoot/genesis/middleware"
+	// geo "github.com/therealfakemoot/genesis/geo"
+	// middleware "github.com/therealfakemoot/genesis/middleware"
 )
 
 func ServeMap(w http.ResponseWriter, r *http.Request) {
-	out := chi.URLParam(r, "target")
+	f, err := os.Open("public/index.html")
+	if err != nil {
+		w.WriteHeader(404)
+		log.WithError(err).Error("couldn't load index file")
+	}
+	io.Copy(w, f)
+}
 
-	m := r.Context().Value(middleware.CtxMap).(geo.Map)
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
 
-	width, height := m.Width, m.Height
-	seed := m.Seed
-	d := m.Domain
+	fs := http.StripPrefix(path, http.FileServer(root))
 
-	m = geo.New(int(width), int(height), int(seed), d)
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
 
-	mapCtx := log.WithFields(log.Fields{
-		"target": out,
-		"seed":   seed,
-		"width":  width,
-		"height": height,
-		"min":    d.Min,
-		"max":    d.Max,
-	})
-
-	mapCtx.Info("serving map")
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
