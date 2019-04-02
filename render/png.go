2a277fd544cf9acfed85298ebf984293c6aad420
Nick Dumas <nicholas.dumas@homes.com>
2019-04-02 13:55:55 +0000

    Upgrade PNG rendering to use new logger middleware.
    Had to tweak function signatures so helpers can get at the logger.
    Otherwise, straightforward.
    
    Also, downgraded the matchColor output to Trace() because it's
    hilariously slow to log all that info.

--- render/png.go
+++ render/png.go
@@ -16,28 +16,28 @@ import (
 	Q "github.com/therealfakemoot/go-quantize"
 )
 
-func matchColor(point float64, d Q.Domain) (c color.Color) {
+func matchColor(point float64, d Q.Domain, logger *log.Entry) (c color.Color) {
 	colorSpace := Q.Domain{
 		Min: 0,
 		Max: 255,
 	}
 	// normalized := uint8(colorSpace.QuantizePoint(point))
 	normalized := uint8(Q.Quantize(point, d, colorSpace))
-	log.WithFields(log.Fields{
+	logger.WithFields(log.Fields{
 		"point": point,
 		"color": normalized,
-	}).Debug("matching color")
+	}).Trace("matching color")
 
 	return color.NRGBA{normalized, normalized, normalized, 255}
 }
 
-func GeneratePNG(m geo.Map) image.Image {
+func GeneratePNG(m geo.Map, logger *log.Entry) image.Image {
 	img := image.NewNRGBA(image.Rect(0, 0, m.Width, m.Height))
 
 	for y := 0; y < m.Height; y++ {
 		for x := 0; x < m.Width; x++ {
 			point := m.Points[x][y]
-			c := matchColor(point, m.Domain)
+			c := matchColor(point, m.Domain, logger)
 			img.Set(x, y, c)
 		}
 	}
@@ -46,20 +46,21 @@ func GeneratePNG(m geo.Map) image.Image {
 }
 
 func ServePNG(w http.ResponseWriter, r *http.Request) {
-	log.Info("png handler")
+	logger := r.Context().Value(middleware.CtxLogger).(*log.Entry)
+	logger.Info("png handler")
 	var err error
 	buffer := new(bytes.Buffer)
 
 	m := r.Context().Value(middleware.CtxMap).(geo.Map)
 	m.Points = geo.Noise(m)
-	i := GeneratePNG(m)
+	i := GeneratePNG(m, logger)
 
 	w.Header().Set("Content-type", "image/png")
 
 	w.Header().Set("Content-Disposition", `inline;filename="butts"`)
 	err = png.Encode(buffer, i)
 	if err != nil {
-		log.WithError(err).Error("image encoding failure")
+		logger.WithError(err).Error("image encoding failure")
 
 		e := struct {
 			Error string
@@ -73,7 +74,7 @@ func ServePNG(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
 	_, err = w.Write(buffer.Bytes())
 	if err != nil {
-		log.WithError(err).Error("response write failure")
+		logger.WithError(err).Error("response write failure")
 	}
 
 }
