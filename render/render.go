158fd01c81bd6c16927b1cfad2238c646304b391
Nick Dumas <dumasn@homes.com>
2019-12-17 10:19:12 -0500

    Dcoumenting render a lil bit

--- render/render.go
+++ render/render.go
@@ -6,6 +6,11 @@ import (
 	geo "github.com/therealfakemoot/genesis/geo"
 )
 
+// RenderFunc will allow for a plug-n-play system.
+//
+// Given a populated geo.Map and an io.Writer, a
+// RenderFunc writes out whatever is appropriate for that
+// representation of the map data.
 type RenderFunc func(w io.Writer, m geo.Map)
 
 /*
