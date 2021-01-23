cd49091621bf2ff748a4babc62ccffe6b5484275
Nick Dumas <nicholas.dumas@homes.com>
2021-01-23 01:58:31 -0500

    Upgrading png rendering demo with flags and junk.

--- cmd/png-viewer/main.go
+++ cmd/png-viewer/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+    "os"
+    "image/png"
+    "flag"
 
     "github.com/therealfakemoot/genesis/render"
-    log "github.com/sirupsen/logrus"
     "github.com/therealfakemoot/genesis/geo"
     Q "github.com/therealfakemoot/go-quantize"
 )
 
 func main() {
-    d := Q.Domain{Min: -1000, Max: 1000}
-    m := geo.New(1000, 1000, 42069, d)
-    i := render.GeneratePNG(m)
+    var (
+        min, max, x, y, seed int
+    )
+
+    flag.IntVar(&min, "min",  -1000, "post-quantize minimum")
+    flag.IntVar(&max, "max", 1000, "post-quantize maximum")
+    flag.IntVar(&x, "x", 1000, "map width")
+    flag.IntVar(&y, "y", 1000, "map height")
+    flag.IntVar(&seed, "seed", 42069, "simplex noise seed")
+
+    d := Q.Domain{Min: float64(min), Max: float64(max)}
+    m := geo.New(x, y, seed, d)
+    // i := render.GeneratePNG(m)
+
+
+
+    png.Encode(os.Stdout, i)
 }
